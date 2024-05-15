edf8f1e20e8d5f9d5e506c6cd397fe3c178288e7
Plamen Michev <plamen@voresforening.dk>
2024-05-15 11:11:26 +0200

    add login and register views

--- src/main/java/dk/via/largedeposit/view/LoginViewController.java
+++ src/main/java/dk/via/largedeposit/view/LoginViewController.java
@@ -1,9 +1,20 @@
 package dk.via.largedeposit.view;
 
 import dk.via.largedeposit.viewmodel.LoginViewModel;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.Region;
 
+import static dk.via.largedeposit.view.ViewFactory.OVERVIEW;
+
 public class LoginViewController {
+    @FXML
+    private TextField emailField;
+
+    @FXML
+    private TextField passwordField;
+
+
     private LoginViewModel viewModel;
     private ViewHandler viewHandler;
     private Region root;
@@ -12,6 +23,19 @@ public class LoginViewController {
         this.viewHandler = viewHandler;
         this.viewModel = viewModel;
         this.root = root;
+    }
+
+    @FXML
+    private void handleLoginButtonAction() {
+        String email = emailField.getText();
+        String password = passwordField.getText();
+
+        viewHandler.openView(ViewFactory.OVERVIEW);
+    }
+
+    @FXML
+    private void handleRegisterButtonAction() {
+        viewHandler.openView(ViewFactory.REGISTER);
     }
 
     public Region getRoot() {
--- src/main/java/dk/via/largedeposit/view/RegisterViewController.java
+++ src/main/java/dk/via/largedeposit/view/RegisterViewController.java
@@ -1,9 +1,43 @@
 package dk.via.largedeposit.view;
 
 import dk.via.largedeposit.viewmodel.RegisterViewModel;
+import javafx.fxml.FXML;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.Region;
 
 public class RegisterViewController {
+
+    @FXML
+    private TextField firstNameField;
+
+    @FXML
+    private TextField lastNameField;
+
+    @FXML
+    private DatePicker dobPicker;
+
+    @FXML
+    private TextField addressField;
+
+    @FXML
+    private TextField postalCodeField;
+
+    @FXML
+    private TextField cityField;
+
+    @FXML
+    private TextField phoneField;
+
+    @FXML
+    private TextField emailField;
+
+    @FXML
+    private TextField passwordField;
+
+    @FXML
+    private TextField cprField;
+
     private RegisterViewModel viewModel;
     private ViewHandler viewHandler;
     private Region root;
@@ -12,6 +46,11 @@ public class RegisterViewController {
         this.viewHandler = viewHandler;
         this.viewModel = viewModel;
         this.root = root;
+    }
+
+    @FXML
+    private void handleRegisterButtonAction() {
+        viewHandler.openView(ViewFactory.OVERVIEW);
     }
 
     public Region getRoot() {
